b4c70e504383bd18f44939eb48fd81efe03ca7be
LTH <johtta888@naver.com>
2022-04-29 20:43:04 +0900

    edit eslint

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,7 @@ import "../styles/globals.css";
 import store from "store";
 
 function MyApp({ Component, pageProps }) {
+  const prefix = process.env.NODE_ENV === 'production' ? '': '';
   return (
     <Provider store={store}>
       <Layout>
